f4f35c58b590268892392936900168dd2e6075d2
Zach Latta <zach@zachlatta.com>
2022-01-24 23:37:01 -0500

    Totally reset this thing, start writing code that actually does something

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,11 @@
-console.log('test!')
+const welcome = document.getElementById('welcome')
+const welcomeButton = document.getElementById('welcomeButton')
+
+const app = document.getElementById('app')
+
+// start the app when they click the welcome button!
+const startApp = () => {
+    welcome.style.display = 'none'
+    app.style.display = 'block'
+}
+welcomeButton.addEventListener('click', startApp)
